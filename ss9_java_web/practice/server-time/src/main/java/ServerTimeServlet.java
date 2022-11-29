80942b72ca2f408f4c08532c0623b4aba4187af4
thuydiem1702 <thuydiem17022001@gmail.com>
2022-11-29 16:53:15 +0700

    up

--- ss9_java_web/practice/server-time/src/main/java/ServerTimeServlet.java
+++ ss9_java_web/practice/server-time/src/main/java/ServerTimeServlet.java
@@ -1,10 +1,23 @@
-@javax.servlet.annotation.WebServlet(name = "ServerTimeServlet")
-public class ServerTimeServlet extends javax.servlet.http.HttpServlet {
-    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+@WebServlet(name = "ServerTimeServlet")
+public class ServerTimeServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
 
-    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
-
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter writer = response.getWriter();
+        writer.println("<html>");
+        Date today = new Date();
+        writer.println("<h1>"+today+"</h1>");
+        writer.println("</html>");
     }
 }
